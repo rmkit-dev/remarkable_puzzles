1ba5cce1be68e2075a607a313e9a92ad96481d8f
Mike Richards <mrichards@democracy.works>
2021-01-14 22:55:33 -0500

    Control drawing debugging with its own define

--- src/puzzles.hpp
+++ src/puzzles.hpp
@@ -19,6 +19,23 @@ extern "C" {
 #include "vendor/puzzles/puzzles.h"
 }
 
+// Debugging
+#if (defined NDEBUG) || !(defined DEBUG_FRONTEND)
+#define dbg_fe(...)
+#define dbg_color(...)
+#else
+#define dbg_fe debugf
+#define dbg_color(color) __dbg_color(handle, color)
+int __dbg_color(void *handle, int color) {
+    int ncolors;
+    float * colors = midend_colours(static_cast<frontend*>(handle)->me, &ncolors);
+    int r = colors[3*color+0] * 255;
+    int g = colors[3*color+1] * 255;
+    int b = colors[3*color+2] * 255;
+    return (r << 16) | (g << 8) | b;
+}
+#endif
+
 // frontend is a (C) typedef struct in puzzles.h; define it as an abstract base
 // class for C++.
 class frontend
@@ -133,44 +150,28 @@ void get_random_seed(void **randseed, int *randseedsize)
 void frontend_default_colour(frontend *fe, float *output)
 {
     fe->frontend_default_colour(output);
-    /* debugf("frontend_default_colour() => (%f, %f, %f)\n", */
-    /*        output[0], output[1], output[2]); */
 }
 
 void deactivate_timer(frontend *fe)
 {
-    debugf("deactivate_timer()\n");
+    dbg_fe("deactivate_timer()\n");
     fe->deactivate_timer();
 }
 
 void activate_timer(frontend *fe)
 {
-    debugf("activate_timer()\n");
+    dbg_fe("activate_timer()\n");
     fe->activate_timer();
 }
 
 
 /** Drawing Api **/
 
-#ifdef NDEBUG
-#define dbg_color(...)
-#else
-#define dbg_color(color) __dbg_color(handle, color)
-int __dbg_color(void *handle, int color) {
-    int ncolors;
-    float * colors = midend_colours(static_cast<frontend*>(handle)->me, &ncolors);
-    int r = colors[3*color+0] * 255;
-    int g = colors[3*color+1] * 255;
-    int b = colors[3*color+2] * 255;
-    return (r << 16) | (g << 8) | b;
-}
-#endif
-
 void cpp_draw_text(void *handle, int x, int y, int fonttype,
                    int fontsize, int align, int colour,
                    const char *text)
 {
-    debugf("draw_text(%d, %d, %d, %d, %d, %x, \"%s\")\n",
+    dbg_fe("draw_text(%d, %d, %d, %d, %d, %x, \"%s\")\n",
            x, y, fonttype, fontsize, align, dbg_color(colour), text);
     static_cast<frontend*>(handle)
         ->draw_text(x, y, fonttype, fontsize, align, colour, text);
@@ -178,13 +179,13 @@ void cpp_draw_text(void *handle, int x, int y, int fonttype,
 
 void cpp_draw_rect(void *handle, int x, int y, int w, int h, int colour)
 {
-    debugf("draw_rect(%d, %d, %d, %d, %x)\n", x, y, w, h, dbg_color(colour));
+    dbg_fe("draw_rect(%d, %d, %d, %d, %x)\n", x, y, w, h, dbg_color(colour));
     static_cast<frontend*>(handle)->draw_rect(x, y, w, h, colour);
 }
 
 void cpp_draw_line(void *handle, int x1, int y1, int x2, int y2, int colour)
 {
-    debugf("draw_line(%d, %d, %d, %d, %x)\n", x1, y1, x2, y2, dbg_color(colour));
+    dbg_fe("draw_line(%d, %d, %d, %d, %x)\n", x1, y1, x2, y2, dbg_color(colour));
     static_cast<frontend*>(handle)->draw_line(x1, y1, x2, y2, colour);
 }
 
@@ -192,20 +193,20 @@ void cpp_draw_polygon(void *handle, int *coords, int npoints,
                       int fillcolour, int outlinecolour)
 {
 #ifndef NDEBUG
-    debugf("draw_polygon(");
-    debugf("[");
+    dbg_fe("draw_polygon(");
+    dbg_fe("[");
     for (int i = 0; i < 2*npoints; i+=2) {
-        debugf("%d, %d", coords[i], coords[i+1]);
+        dbg_fe("%d, %d", coords[i], coords[i+1]);
         if (i < 2*(npoints-1))
-            debugf(",");
+            dbg_fe(",");
     }
-    debugf("], ");
-    debugf("%d, ");
+    dbg_fe("], ");
+    dbg_fe("%d, ");
     if (fillcolour == -1)
-        debugf("-1, ");
+        dbg_fe("-1, ");
     else
-        debugf("%x, ", dbg_color(fillcolour));
-    debugf("%x)\n", dbg_color(outlinecolour));
+        dbg_fe("%x, ", dbg_color(fillcolour));
+    dbg_fe("%x)\n", dbg_color(outlinecolour));
 #endif
     static_cast<frontend*>(handle)
         ->draw_polygon(coords, npoints, fillcolour, outlinecolour);
@@ -215,12 +216,12 @@ void cpp_draw_circle(void *handle, int cx, int cy, int radius,
                      int fillcolour, int outlinecolour)
 {
 #ifndef NDEBUG
-    debugf("draw_circle(%d, %d, %d, ", cx, cy, radius);
+    dbg_fe("draw_circle(%d, %d, %d, ", cx, cy, radius);
     if (fillcolour == -1)
-        debugf("-1, ");
+        dbg_fe("-1, ");
     else
-        debugf("%x, ", dbg_color(fillcolour));
-    debugf("%x)\n", dbg_color(outlinecolour));
+        dbg_fe("%x, ", dbg_color(fillcolour));
+    dbg_fe("%x)\n", dbg_color(outlinecolour));
 #endif
     static_cast<frontend*>(handle)
         ->draw_circle(cx, cy, radius, fillcolour, outlinecolour);
@@ -231,7 +232,7 @@ void cpp_draw_thick_line(void *handle, float thickness,
                          float x1, float y1, float x2, float y2,
                          int colour)
 {
-    debugf("draw_thick_line(%f, %f, %f, %f, %f, %x)\n",
+    dbg_fe("draw_thick_line(%f, %f, %f, %f, %f, %x)\n",
            thickness, x1, x2, y1, y2, dbg_color(colour));
     static_cast<frontend*>(handle)
         ->draw_thick_line(thickness, x1, y1, x2, y2, colour);
@@ -239,63 +240,63 @@ void cpp_draw_thick_line(void *handle, float thickness,
 
 void cpp_draw_update(void *handle, int x, int y, int w, int h)
 {
-    debugf("draw_update(%d, %d, %d, %d)\n", x, y, w, h);
+    dbg_fe("draw_update(%d, %d, %d, %d)\n", x, y, w, h);
     static_cast<frontend*>(handle)->draw_update(x, y, w, h);
 }
 
 void cpp_clip(void *handle, int x, int y, int w, int h)
 {
-    debugf("clip(%d, %d, %d, %d)\n", x, y, w, h);
+    dbg_fe("clip(%d, %d, %d, %d)\n", x, y, w, h);
     static_cast<frontend*>(handle)->clip(x, y, w, h);
 }
 
 void cpp_unclip(void *handle)
 {
-    debugf("unclip()\n");
+    dbg_fe("unclip()\n");
     static_cast<frontend*>(handle)->unclip();
 }
 
 void cpp_start_draw(void *handle)
 {
-    debugf("start_draw()\n");
+    dbg_fe("start_draw()\n");
     static_cast<frontend*>(handle)->start_draw();
 }
 
 void cpp_end_draw(void *handle)
 {
-    debugf("end_draw()\n");
+    dbg_fe("end_draw()\n");
     static_cast<frontend*>(handle)->end_draw();
 }
 
 void cpp_status_bar(void *handle, const char *text)
 {
-    debugf("status_bar(\"%s\")\n", text);
+    dbg_fe("status_bar(\"%s\")\n", text);
     static_cast<frontend*>(handle)->status_bar(text);
 }
 
 blitter * cpp_blitter_new(void *handle, int w, int h)
 {
-    debugf("blitter_new(%d, %d) => ");
+    dbg_fe("blitter_new(%d, %d) => ");
     blitter * bl = static_cast<frontend*>(handle)->blitter_new(w, h);
-    debugf("%p\n", (void*)bl);
+    dbg_fe("%p\n", (void*)bl);
     return bl;
 }
 
 void cpp_blitter_free(void *handle, blitter *bl)
 {
-    debugf("blitter_free(%p)\n", (void*)bl);
+    dbg_fe("blitter_free(%p)\n", (void*)bl);
     static_cast<frontend*>(handle)->blitter_free(bl);
 }
 
 void cpp_blitter_save(void *handle, blitter *bl, int x, int y)
 {
-    debugf("blitter_save(%p, %d, %d)\n", (void*)bl, x, y);
+    dbg_fe("blitter_save(%p, %d, %d)\n", (void*)bl, x, y);
     static_cast<frontend*>(handle)->blitter_save(bl, x, y);
 }
 
 void cpp_blitter_load(void *handle, blitter *bl, int x, int y)
 {
-    debugf("blitter_load(%p, %d, %d)\n", (void*)bl, x, y);
+    dbg_fe("blitter_load(%p, %d, %d)\n", (void*)bl, x, y);
     static_cast<frontend*>(handle)->blitter_load(bl, x, y);
 }
 
