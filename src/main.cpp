f694eaccd24a6cf39f61f2d44b985bca87ac8bb1
Mike Richards <mrichards@democracy.works>
2021-02-02 23:13:38 -0500

    Auto-save 2-seconds after input events

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <memory>
 
 #include <rmkit.h>
@@ -7,9 +8,13 @@
 #include "timer.hpp"
 #include "ui/game_scene.hpp"
 
+const double AUTO_SAVE_INTERVAL_S = 2.0;
+
 class App {
 public:
     std::shared_ptr<GameScene> game_scene;
+    double last_save = 0.0;
+    bool wants_auto_save = true;
 
     App()
     {
@@ -26,15 +31,33 @@ public:
         game_scene->show();
     }
 
+    void check_auto_save()
+    {
+        // set wants_auto_save after every input event, assuming it might have
+        // changed the game's state
+        wants_auto_save = wants_auto_save || !ui::MainLoop::in.all_motion_events.empty();
+        if (wants_auto_save && timer::now() - last_save > AUTO_SAVE_INTERVAL_S) {
+            if (game_scene->save_state()) {
+                last_save = timer::now();
+                std::cerr << "auto save" << std::endl;
+            }
+            wants_auto_save = false;
+        }
+    }
+
     void run()
     {
         while (1) {
+            check_auto_save();
             game_scene->check_timer();
             game_scene->check_solved();
             // Process events and redraw
             ui::MainLoop::main();
             ui::MainLoop::redraw();
-            ui::MainLoop::read_input(game_scene->wants_timer() ? timer::INTERVAL_MS : 0);
+            ui::MainLoop::read_input(
+                    game_scene->wants_timer() ? timer::INTERVAL_MS
+                    : wants_auto_save ? AUTO_SAVE_INTERVAL_S * 1000
+                    : 0);
         }
     }
 };
